565b9b10d2ecf16a0c65e978a480a9898091f909
Matheus Lessa Fumian <matheusfumian@gmail.com>
2020-11-04 15:28:55 -0300

    carousel with stacks

---
+++ carousel-techs.php
@@ -0,0 +1,9 @@
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2>carrossel com as tecnologias que sei</h2>
+            </div>
+        </div>
+    </div>
+</section>
