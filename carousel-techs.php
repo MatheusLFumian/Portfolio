d40d148f4906ff125a993d458a924814cc5fb460
Matheus Lessa Fumian <matheusfumian@gmail.com>
2020-11-04 16:02:14 -0300

    version html

--- carousel-techs.php
+++
@@ -1,9 +0,0 @@
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h2>carrossel com as tecnologias que sei</h2>
-            </div>
-        </div>
-    </div>
-</section>
